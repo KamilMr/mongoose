6600f2feeaa4351b0550cb389660b00add185083
Kamil Mrowka <kamil@chicos.life>
2024-02-08 19:35:18 +0100

    Add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const userRoutes = require('./users.routes');
+const mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb+srv://kmrowka:kmrowka@cluster0.nthxqyo.mongodb.net/contacs?retryWrites=true&loadBalanced=false&replicaSet=atlas-9teg12-shard-0&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1')
+db.then(() => {
+  console.log('Db is running');
+}).catch(err => {
+  console.log(err)
+})
 
 const app = express();
 
